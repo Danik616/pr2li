9ef3ac45c738c3aa7ffc1b2faae9d71162f6f2ec
Danik616 <ddanielvargas2011@gmail.com>
2023-05-09 16:00:26 -0500

    Se hacen dos metodos, uno en userRepository y otro en RolRepository

--- src/main/java/com/pruebas/liti/Repository/IUserProofRepository.java
+++ src/main/java/com/pruebas/liti/Repository/IUserProofRepository.java
@@ -1,9 +1,13 @@
 package com.pruebas.liti.Repository;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
 import com.pruebas.liti.entity.UserEntityProof;
 
+import reactor.core.publisher.Mono;
+
 public interface IUserProofRepository extends R2dbcRepository<UserEntityProof, Long>{
-    
+    @Query("SELECT * FROM usuario WHERE email =:email")
+    public Mono<UserEntityProof> findByEmail(String email);
 }
--- src/main/java/com/pruebas/liti/Repository/RolUsuarioRepository.java
+++ src/main/java/com/pruebas/liti/Repository/RolUsuarioRepository.java
@@ -1,9 +1,15 @@
 package com.pruebas.liti.Repository;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
 import com.pruebas.liti.entity.RolUsuarioEntity;
 
+import reactor.core.publisher.Flux;
+
+
 public interface RolUsuarioRepository extends R2dbcRepository<RolUsuarioEntity, Long>{
     
+    @Query("SELECT * FROM rol_usuario WHERE usuario_id = :id")
+    public Flux<RolUsuarioEntity> findByUserId(long id);
 }
