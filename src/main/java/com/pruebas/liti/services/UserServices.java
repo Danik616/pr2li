4ca8600bd15fc0024ffea75dc3d5c04bc2f8f32c
Danik616 <ddanielvargas2011@gmail.com>
2023-05-10 12:22:49 -0500

    Se crea la funcion de findByUsername que asemeja al loadByUsername, solo que aquí se hace de forma reactiva

--- src/main/java/com/pruebas/liti/services/UserServices.java
+++ src/main/java/com/pruebas/liti/services/UserServices.java
@@ -1,10 +1,20 @@
 package com.pruebas.liti.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.pruebas.liti.Repository.IRolProofRepository;
 import com.pruebas.liti.Repository.IUserProofRepository;
+import com.pruebas.liti.Repository.RolUsuarioRepository;
+import com.pruebas.liti.entity.RolEntityProof;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class UserServices implements IUserServices{
@@ -12,9 +22,31 @@ public class UserServices implements IUserServices{
     @Autowired
     public IUserProofRepository userRepository;
 
+    @Autowired
+    public IRolProofRepository rolRepository;
+
+    @Autowired
+    public RolUsuarioRepository rolUsuarioRepository;
+
     @Override
-    public Mono<UserDetails> findByUsername(String username) {
-        throw new UnsupportedOperationException("Unimplemented method 'findByUsername'");
+    public Mono<UserDetails> findByUsername(String email) {
+        return userRepository.findByEmail(email).flatMap( account -> {
+            Flux<RolEntityProof> userBuilderMono = 
+                rolUsuarioRepository.findByUserId(account.getId())
+                    .flatMap(rolUsuario -> rolRepository.findById(rolUsuario.getRolId()));
+
+                    return userBuilderMono.collectList().map(roles -> {
+                        List<GrantedAuthority> authorities = roles.stream()
+                                .map(rol -> new SimpleGrantedAuthority(rol.getNombre()))
+                                .collect(Collectors.toList());
+            
+                        return User.builder()
+                                .username(account.getEmail())
+                                .password(account.getPassword())
+                                .authorities(authorities)
+                                .build();
+                    });
+        });
     }
     
 }
