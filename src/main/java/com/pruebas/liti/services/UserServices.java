d989a9a61252718a64899ce750c6a6a1556106d7
Danik616 <ddanielvargas2011@gmail.com>
2023-05-10 11:27:57 -0500

    Se añade el repositorio al servicio, se añade plugin para que se vea bonito

--- src/main/java/com/pruebas/liti/services/UserServices.java
+++ src/main/java/com/pruebas/liti/services/UserServices.java
@@ -1,10 +1,16 @@
 package com.pruebas.liti.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.pruebas.liti.Repository.IUserProofRepository;
 
 import reactor.core.publisher.Mono;
 
 public class UserServices implements IUserServices{
+
+    @Autowired
+    public IUserProofRepository userRepository;
 
     @Override
     public Mono<UserDetails> findByUsername(String username) {
