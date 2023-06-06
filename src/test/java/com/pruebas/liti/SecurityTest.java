eb5f88b1d0eb13a3b332505661058234878d6e7e
Danik616 <ddanielvargas2011@gmail.com>
2023-06-06 16:58:38 -0500

    se cambian tests

--- src/test/java/com/pruebas/liti/SecurityTest.java
+++ src/test/java/com/pruebas/liti/SecurityTest.java
@@ -4,16 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import static org.mockito.Mockito.*;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.pruebas.liti.Repository.IRolProofRepository;
@@ -29,8 +21,7 @@ import reactor.test.StepVerifier;
 @ActiveProfiles("test")
 public class SecurityTest {
 
-    @Autowired
-    private ReactiveUserDetailsService userDetailsService;
+
 
     @Mock
     private UserServices userService;
@@ -40,53 +31,6 @@ public class SecurityTest {
 
     @Autowired
     IRolProofRepository rolRepository;
-
-//     @Test
-//     @WithMockUser(username = "prueba3@gmail.com", password = "Test12345")
-//     public void testAuthenticate() {
-
-
-//         // Call the authentication manager and expect a successful authentication
-//         Mono<UserDetails> userDetailsMono = userDetailsService.findByUsername("prueba3@gmail.com");
-//         StepVerifier.create(userDetailsMono)
-//                 .assertNext(userDetails -> {
-//                     boolean matches = passwordEncoder.matches("Test12345", userDetails.getPassword());
-//                     assert matches : "Password does not match";
-//                 })
-//                 .expectComplete()
-//                 .verify();
-//     }
-
-//     /**
-//      * 
-//      */
-//     @Test
-// void testAboutAuthenticateUDRepository() {
-//     // Crea un usuario ficticio con credenciales válidas
-//     String email = "prueba3@gmail.com";
-//     String password = "Test12345";
-//     UserDetails user = User.builder()
-//             .username(email)
-//             .password(passwordEncoder.encode(password))
-//             .roles("GUESS")
-//             .build();
-    
-//     // Crea un UserDetailsRepositoryReactiveAuthenticationManager y configúralo con un UserDetailsRepository
-//     UserServices userDetailsRepository = mock(UserServices.class);
-//     when(userDetailsRepository.findByUsername(email)).thenReturn(Mono.just(user));
-//     UserDetailsRepositoryReactiveAuthenticationManager authManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsRepository);
-//     authManager.setPasswordEncoder(new BCryptPasswordEncoder());
-
-//     // Crea un AuthenticationToken con las credenciales del usuario ficticio
-//     Authentication auth = new UsernamePasswordAuthenticationToken(email, password);
-
-//     // Prueba que la autenticación tenga éxito
-//     StepVerifier.create(authManager.authenticate(auth))
-//             .expectNextMatches(authentication -> authentication.isAuthenticated())
-//             .expectComplete()
-//             .verify();
-// }
-
 
 @Test
 public void testRepository(){
